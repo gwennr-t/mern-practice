0f45ec2eb1bc80a8d6ca1baab3f818e5c5872dad
gwennr-t <gwennt321@gmail.com>
2024-05-09 19:40:55 -0500

    changing working on profile page to remove likes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -44,7 +44,7 @@ const Profile = () => {
   return (
     <div>
       <h2 className="card-header">
-        {profileId ? `${profile.name}'s` : 'Your'} friends have liked these
+        {profileId ? `${profile.name}'s` : 'Your'} friends have added these
         genres...
       </h2>
 
